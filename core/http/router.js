99815e68f6644543583f553d7cd32e5ec26224d4
Ben Osborne <bosborne@sunspottours.com>
2018-04-09 08:35:19 +0100

    Added dynamic routing.

--- core/http/router.js
+++ core/http/router.js
@@ -128,18 +128,98 @@ module.exports = class Router {
                     // Get url
                     const route_url = route.url.toString().toLowerCase().trim();
 
+                    // Add the data layer
+                    route['data_layer'] = {};
+
                     // Route Match
                     if(route_url == this.url_parts.pathname){
-
-                        // Add the data layer
-                        route['data_layer'] = {};
 
                         // Resolve
                         resolve(route);
 
                     }
 
-                    // Dynamic placeholder match
+                    /**
+                     *
+                     * Dynamic Routing
+                     *
+                     */
+
+                    // Get route parameters
+                    let route_matched_pattern = route_url.match(/(:[a-zA-Z_-]+)/g);
+
+                    // Check if we matched any parameters in the routes path
+                    if('undefined' !== typeof route_matched_pattern){
+                        if('object' == typeof route_matched_pattern && route_matched_pattern && Object.keys(route_matched_pattern).length > 0){
+
+                            // Split the current URL into segments
+                            let url_segments = this.url_parts.pathname.toString().split("/");
+
+                            // Get the route segments
+                            let route_segments = route_url.toString().split("/");
+                            let real_path = [];
+
+                            // Set counter
+                            let counter = 0;
+
+                            // Iterate over each segment
+                            if(url_segments && url_segments.length > 0){
+
+                                // Iterate
+                                url_segments.forEach(segment => {
+                                    segment = segment.toString().replace(' ','').trim();
+                                    if(segment !== ''){
+
+                                        // Check if this route segment if exists starts with :
+                                        if('undefined' !== typeof route_segments[counter]){
+
+                                            // Match a placeholder
+                                            if(route_segments[counter].match(/^:/)){
+
+                                                // Replace the route segment with the actual value
+                                                real_path[counter] = segment;
+
+                                                // Add to data layer
+                                                route['data_layer'][route_segments[counter].replace(':','')] = segment;
+
+                                            }else{
+
+                                                // Check if current segment of URL matches the route segment
+                                                if(route_segments[counter] == segment){
+
+                                                    // Matched a normal segment
+                                                    real_path.push(segment);
+
+                                                }
+
+                                            }
+
+                                        }
+
+                                    }else{
+
+                                        // Push an empty string
+                                        real_path.push("");
+
+                                    }
+
+                                    // Increment
+                                    counter++;
+
+                                });
+
+                                // Check if real_path if good
+                                if(real_path.length == route_segments.length){
+
+                                    // The route matches
+                                    resolve(route);
+
+                                }
+
+                            }
+
+                        }
+                    }
 
                 }
             }
